b4659714535136beb102e91cd4f04c16395cdaee
Ayush Kumar Pandit <ayushpanditmoto@gmail.com>
2022-12-31 00:40:00 +0530

    feat: mobile responsive

---
+++ src/Components/Footer.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="contacts">
+      <h2>Contact Me</h2>
+
+      <div className="ids">
+        <a
+          href="https://www.linkedin.com/in/sayandeep-sarkar-530688201/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Linked In
+        </a>
+        <a
+          href="https://github.com/Sayandeep27"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github
+        </a>
+        <a
+          href="https://www.instagram.com/sayandeep_19_2002/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Instagram
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
